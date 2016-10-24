79fcceed20eb55b705c26dbd08f67456dfc1ffa4
Michael Glenn <dmichaelglenn@gmail.com>
2016-10-24 14:32:20 -0700

    separated ang routes into their own config file

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,13 +1,1 @@
-angular.module("BeatupApp", ["ngRoute"])
-
-.config(function($routeProvider){
-  $routeProvider
-  .when("/main", {
-    templateUrl: "./templates/main.html",
-    controller: "MainController"
-  })
-  .otherwise("/main", {
-      templateUrl: "./templates/main.html",
-        controller: "MainController"
-  });
-})
+var app = angular.module("BeatupApp", ["ngRoute"]);
