b346576f73fdc5697209ec5b19155e74368f40af
Devin Mertz <devinjmertz@gmail.com>
2016-10-30 16:58:30 -0600

    event controller and event service

---
+++ frontend/services/eventService.js
@@ -0,0 +1,25 @@
+angular.module('BeatupApp')
+	.service("EventService", ['$http', 'UserService', function ($http, UserService){
+		this.loggedInUser = UserService.loggedInUser;
+		var _this = this;
+		var baseUrl = "http://localhost:8000";
+		
+		//adding user (yourself) as a member to an event//
+		this.joinEvent = function(eventObj) {
+			return $http.post(baseUrl + '/api/event/members', eventObj._id)
+			.then(function(response) {
+				_this.loggedInUser = UserService.loggedInUser;
+				_this.Event.push(_this.loggedInUser.username);
+				return response.data;
+			});
+		};
+		
+		//adding comment to event page//
+		this.addComment = function(eventObj, comment) {
+			return $http.post(baseUrl + 'api/event/comments', eventObj._id, comment).then(function(reponse) {
+				_this.Event.comments.push(response.data);
+				return response.data;
+			});
+		};
+	}]);
+
